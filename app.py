a3515f4da135fc00b0026876b5e971ecf07f6253
iamebube-ekpe <victorekpe11@gmail.com>
2021-09-10 02:02:12 +0100

    updated html file and added axios js file

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from flask_restful import Api, reqparse
 from flask_cors import CORS
+import tweepy as twt
+from textblob import TextBlob
+from wordcloud import WordCloud
+import pandas as pd
+import numpy as np
+import re
+# import matplotlib.pyplot as plt
+# plt.style.use('fivethirtyeight')
+
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
+CORS(app, resources=r'/api/*')
 
-@app.route('/', methods=['GET'])
+tweets = [ "qqwwwweerrttt", "ashhdskdjkksadjas", "asdsakdoiahdjfsfoijdas" ]
+
+def analyzeSentiment(twtName, numTwts):
+    pass
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
+@app.route('/api/analyze', methods=['POST'])
+def sentiment():
+    parser = reqparse.RequestParser()
+    parser.add_argument('twtId',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!")
+    parser.add_argument('numOfTwts',
+        type=int,
+        required=True,
+        help="This field cannot be left blank!")
+
+    if request.method == 'POST':
+        args = parser.parse_args()
+        twtId = args['twtId']
+        numOfTwts = args['numOfTwts']
+
+        print(f'{twtId}, {numOfTwts}')
+
+    return jsonify({'message': 'success'})
 
 
 
--- static/main.js
+++ static/main.js
@@ -1,24 +1,83 @@
 var NavBr = Vue.component('shownav', {
-    data: function() {
+    data: function () {
         return {}
     },
     template: `
     <div>
         <nav class="navbar navbar-dark bg-secondary">
             <div class="container-fluid">
-                <a href="" class="navbar-brand">Navbar</a>
+                <a href="" class="navbar-brand">EVEL</a>
             </div>
         </nav>
     </div>
     `
 });
 
+var Form = Vue.component('display-form', {
+    data: function () {
+        return {
+            form: {
+                twtId: '',
+                numOfTwts: 0
+            }
+        }
+    },
+    methods: {
+        async submitForm() {
+            if (this.form.twtId === '') {
+                alert('Please enter a twitter id');
+                return;
+            }
+            if (this.form.numOfTwts === 0) {
+                alert('Please enter number of tweets');
+                return;
+            } else if (this.form.numOfTwts > 10) {
+                alert(`Please enter a number less than ${this.form.numOfTwts}`);
+                return;
+            }
+            await axios.post('http://localhost:5000/api/analyze', this.form)
+                .then(response => {
+                    console.log(response);
+                    this.form.twtId = '';
+                    this.form.numOfTwts = 0;
+                })
+
+        }
+    },
+    template: `<div class="container-fluid">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-sm-6 card-body" style="width: 15rem;">
+                <form id='twtForm' @submit.prevent='submitForm()'>
+                    <!-- Name input -->
+                    <div class="form-outline mb-4">
+                        <input type="text" id="form5Example1" class="form-control" autocomplete='off' v-model='form.twtId'/>
+                        <label class="form-label" for="form5Example1">ID/Name Of Twitter Account</label>
+                    </div>
+
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                        <input type="number" id="form5Example2" class="form-control" autocomplete='off' v-model='form.numOfTwts' />
+                        <label class="form-label" for="form5Example2">Number Of Tweets to process (Maximum 10)</label>
+                    </div>
+
+                    <!-- Submit button -->
+                    <button type="submit" class="btn btn-primary btn-block mb-4">Send</button>
+                </form>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+`
+})
+
 
 
 var app = new Vue({
     el: '#app',
     components: {
-        'shownav': NavBr
+        'shownav': NavBr,
+        'display-form': Form
     }
 
 });
